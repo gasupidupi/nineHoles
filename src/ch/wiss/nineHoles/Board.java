585ff8cc4720a403792eea860258390c799e2952
gasupidupi <gasupidupi@gmail.com>
2018-11-16 17:57:36 +0100

    Added ConsoleReader and field method

--- src/ch/wiss/nineHoles/Board.java
+++ src/ch/wiss/nineHoles/Board.java
@@ -1,5 +1,11 @@
 package ch.wiss.nineHoles;
 
 public class Board {
-
+	static final char PIECE_X = 'X';
+	static final char PIECE_Y = 'O';
+	private char[][] field = new char[3][3];
+	
+	public void setPiece(int x, int y, char piece) {
+		field[x][y] = piece;
+	}
 }
--- src/ch/wiss/nineHoles/Player.java
+++ src/ch/wiss/nineHoles/Player.java
@@ -2,4 +2,13 @@ package ch.wiss.nineHoles;
 
 public class Player {
 
+	private Integer numPieces = 0;
+	
+	public void nextDrop() {
+		
+	}
+	
+	public void nextMove() {
+		
+	}
 }
