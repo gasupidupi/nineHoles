e95a6d68dd34d96bc50376ce06367865764374b5
gasupidupi <gasupidupi@gmail.com>
2018-11-17 10:02:01 +0100

    Moving phase
    WARNING:
    field[y+1][x] == EMPTY	<error(s)_during_the_evaluation>
    => BUG: CAN'T MOVE

--- src/ch/wiss/nineHoles/Human.java
+++ src/ch/wiss/nineHoles/Human.java
@@ -2,8 +2,13 @@ package ch.wiss.nineHoles;
 
 public class Human extends Player{
 	
-
-
+	private String errorMsg = "##################ERROR:OCCUPIED OR NOT ON BOARD##################";
+	private String yPosMsg = String.format("Player %c Y-Pos?", piece);
+	private String xPosMsg = String.format("Player %c X-Pos?", piece);
+	private String directionMsg = "Which direction(u,d,r,l)? ";
+	private String direction;
+	private int x;
+	private int y;
 	
 	Human(Board board, char piece) {
 		super(board, piece);
@@ -13,22 +18,53 @@ public class Human extends Player{
 	@Override
 	public void nextDrop() {
 		// TODO Auto-generated method stub
-		int y = ConsoleReader.readInteger(String.format("Player %c Y-Pos?", piece));
-		int x = ConsoleReader.readInteger(String.format("Player %c X-Pos?", piece));
+		int y = ConsoleReader.readInteger(yPosMsg);
+		int x = ConsoleReader.readInteger(xPosMsg);
 		if(!board.setPiece(y, x, piece)) {
-			System.out.println("##################ERROR:OCCUPIED OR NOT ON BOARD##################");
+			System.out.println(errorMsg);
 			nextDrop();
 		}
 	}
 
+	
+	//Maybe move code-segment to Player
 	@Override
 	public void nextMove() {
 		// TODO Auto-generated method stub
-		
+		y = ConsoleReader.readInteger(yPosMsg);
+		x = ConsoleReader.readInteger(xPosMsg);
+		direction = ConsoleReader.readString(directionMsg);
+		switch (direction){
+		case "u":
+			if(!board.moveUp(y, x, piece)) {
+				System.out.println(errorMsg);
+				nextMove();
+			}
+			break;
+		case "d":
+			if(!board.moveDown(y, x, piece)) {
+				System.out.println(errorMsg);
+				nextMove();
+			}
+			break;
+		case "r":
+			if(!board.moveRight(y, x, piece)) {
+				System.out.println(errorMsg);
+				nextMove();
+			}
+			break;
+		case "l":
+			if(!board.moveLeft(y, x, piece)) {
+				System.out.println(errorMsg);
+				nextMove();
+			}
+			break;
+		default:
+			nextMove();
+		}
 	}
 
-	
- 
+
 	
 	
 
