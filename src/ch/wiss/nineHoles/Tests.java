9fcf690fa5c924a0d50f53acb6c2352496e2fa14
gasupidupi <gasupidupi@gmail.com>
2018-11-19 20:18:44 +0100

    junit

--- src/ch/wiss/nineHoles/Tests.java
+++ src/ch/wiss/nineHoles/Tests.java
@@ -6,20 +6,46 @@ import org.junit.jupiter.api.Test;
 
 class Tests {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+
 
 
     @Test
-    public void checkPiecesAmount() {
+    public void occupiedFieldNotEmpty() {
         Board tester = new Board();
         tester.setPiece(0, 0, 'O');
 
         assertEquals(false, tester.isFieldEmpty(0, 0), "Occupied field should not be empty.");
+
+
+    }
+    
+    @Test
+    public void notOccupiedFieldEmpty() {
+        Board tester = new Board();
+        tester.setPiece(0, 0, 'O');
+
+
         assertEquals(true, tester.isFieldEmpty(1, 1), "Not occupied field should be empty.");
-        assertEquals(true, tester.isDirectionEmpty(0, 0, 'u'), "Unavailable direction should not be available.");
+
+
+    }
+    
+    @Test
+    public void unavailableDirectionNotAvailable() {
+        Board tester = new Board();
+        tester.setPiece(0, 0, 'O');
+
+        assertEquals(false, tester.isDirectionEmpty(0, 0, 'u'), "Unavailable direction should not be available.");
+
+
+    }
+    
+    @Test
+    public void availableDirectionAvailable() {
+        Board tester = new Board();
+        tester.setPiece(0, 0, 'O');
+
+
         assertEquals(true, tester.isDirectionEmpty(0, 0, 'd'), "Available direction should be available.");
 
     }
